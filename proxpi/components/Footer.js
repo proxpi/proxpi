66b85eaf95fc14cacd3a0093a59ae77ec63b1bf7
Fredy Somy <fredysomy@gmail.com>
2021-07-30 22:38:59 +0530

    added uptimerobot widget

--- proxpi/components/Footer.js
+++ proxpi/components/Footer.js
@@ -1,122 +1,141 @@
 import React, { Component, useEffect } from "react";
+import axios from "axios";
 import "../assets/footer.css";
-const Footer = () => (
-  <footer style={{ margin: "1% 4.5%" }} class="pt-4 my-md-5 pt-md-5 border-top">
-    <div class="row">
-      <div class="col-12 col-md">
-        <img
-          class="mb-2"
-          src="https://avatars.githubusercontent.com/u/84428745?s=400&u=4c7cc5b97236c44f0aea44f2b477cdee20e13953&v=4"
-          alt=""
-          width="80"
-          height="80"
-        />
+import StatusWidget from "./sub_components/StatusWidget";
+const Footer = () => {
+  return (
+    <footer
+      style={{ margin: "1% 4.5%" }}
+      class="pt-4 my-md-5 pt-md-5 border-top"
+    >
+      <div class="row">
+        <div class="col-12 col-md">
+          <img
+            class="mb-2"
+            src="https://avatars.githubusercontent.com/u/84428745?s=400&u=4c7cc5b97236c44f0aea44f2b477cdee20e13953&v=4"
+            alt=""
+            width="80"
+            height="80"
+          />
 
-        <hr></hr>
-        <div
-          class="socialicons"
-          style={{ display: "flex", flexDirection: "row" }}
-        >
-          <a href="https://github.com/proxpi">
-            <i class="fab fa-github fa-2x"></i>
-          </a>
-          <a href="https://instagram.com/proxpi.tech">
-            <i class="fab fa-instagram fa-2x"></i>
-          </a>
-          <a href="https://twitter.com/proxpi_tech">
-            <i class="fab fa-twitter fa-2x"></i>
-          </a>
-          <a href="https://linkedin.com/company/proxpi">
-            <i class="fab fa-linkedin fa-2x"></i>
-          </a>
+          <hr></hr>
+          <div
+            class="socialicons"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <a href="https://github.com/proxpi">
+              <i class="fab fa-github fa-2x"></i>
+            </a>
+            <a href="https://instagram.com/proxpi.tech">
+              <i class="fab fa-instagram fa-2x"></i>
+            </a>
+            <a href="https://twitter.com/proxpi_tech">
+              <i class="fab fa-twitter fa-2x"></i>
+            </a>
+            <a href="https://linkedin.com/company/proxpi">
+              <i class="fab fa-linkedin fa-2x"></i>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-6 col-md">
+          <hr></hr>
+          <h5>Product</h5>
+          <ul class="list-unstyled text-small">
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                ProxPi
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Access Tokens(Comming soon)
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <hr></hr>
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Documentation
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Blog
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Analytics
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                IP/URL Blocking
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                OSS Guide
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <hr></hr>
+          <h5>Developers</h5>
+          <ul class="list-unstyled text-small">
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Open Source
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Contributors
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Team
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Contributing Guide
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="#">
+                Code documentation
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <div class="col-6 col-md">
-        <hr></hr>
-        <h5>Product</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              ProxPi
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Access Tokens(Comming soon)
-            </a>
-          </li>
-        </ul>
+      <hr></hr>
+      <h6 style={{ textAlign: "center" }} class="d-block mb-3 text-muted">
+        <i class="far fa-copyright"></i> {new Date().getFullYear()} ProxPi
+      </h6>
+      <h6 style={{ textAlign: "center" }}>
+        Made with <span style={{ color: "#e25555" }}>&#9829;</span> in
+        Kerala,India
+      </h6>
+      <div
+        style={{
+          alignContent: "center",
+          display: "flex",
+          justifyContent: "center",
+          margin: "2%",
+        }}
+      >
+        <StatusWidget />
       </div>
-      <div class="col-6 col-md">
-        <hr></hr>
-        <h5>Resources</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Documentation
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Blog
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Analytics
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              IP/URL Blocking
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              OSS Guide
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <hr></hr>
-        <h5>Developers</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Open Source
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Contributors
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Team
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Contributing Guide
-            </a>
-          </li>
-          <li class="mb-1">
-            <a class="link-secondary text-decoration-none" href="#">
-              Code documentation
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <hr></hr>
-    <h6 style={{ textAlign: "center" }} class="d-block mb-3 text-muted">
-      <i class="far fa-copyright"></i> {new Date().getFullYear()} ProxPi
-    </h6>
-    <h6 style={{textAlign:"center"}}>
-    Made with <span style={{color: "#e25555"}}>&#9829;</span> in Kerala,India</h6>
-  </footer>
-);
+    </footer>
+  );
+};
 export default Footer;
---
+++ proxpi/components/sub_components/StatusWidget.js
@@ -0,0 +1,48 @@
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
+
+const StatusWidget = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios.get("https://proxpi-status-page.deta.dev/").then((data) => {
+      setData(data.data);
+      setLoading(false);
+    });
+  }, []);
+  if (loading) {
+    return <h6>Loading</h6>;
+  } else {
+    return (
+     
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "fit-content",
+          height: "fit-content",
+          color: "aqua",
+          justifyContent: "center",
+          backgroundColor: "rgb(23, 28, 35)",
+          margin: "2%",
+          padding: "1%",
+        }} 
+      >
+        <h5  style={{ marginBottom: "0px" }} id="amsg">
+          <img
+            width="50px"
+            height="50px"
+            src={
+              data.state == "success"
+                ? "https://raw.githubusercontent.com/proxpi/images/main/successdot.png"
+                : "https://raw.githubusercontent.com/proxpi/images/main/dangerdot.png"
+            }
+          ></img>{" "}
+          <a target="_blank" href="https://stats.uptimerobot.com/poBOBizV5q">{data.message}</a>
+        </h5>
+      </div>
+      
+    );
+  }
+};
+export default StatusWidget;
